b984dedc033b74bde37e9e5455b7a0400b0afba2
joshii04 <joshua.sinclair04@gmail.com>
2023-05-19 12:38:43 +0100

    creating the method class and main program tester
    done

--- assignmentTwo/StockItem2.java
+++ assignmentTwo/StockItem2.java
@@ -9,4 +9,70 @@ public class StockItem2
     private int quantity;
     private int reOrderLevel;
 
+    public StockItem2(String itemID, String itemDesc, double price, int quantity, int reOrderLevel)
+    {   //this is the constructor
+        //instance variable last
+
+        this.itemID = itemID;
+        this.itemDesc = itemDesc;
+        this.price = price; 
+        this.quantity = quantity;
+        this.reOrderLevel = reOrderLevel;
+    }
+
+    //create the gets and sets 
+
+    public String getItemID()
+    {
+        return this.itemID;
+    }
+
+    public String getItemDesc()
+    {
+        return this.itemDesc; 
+    }
+
+    public double getPrice()
+    {
+        return this.price;
+    }
+
+    public void setPrice(double newPrice)
+    {
+        // instance this. var first
+
+        this.price = newPrice;
+    }
+
+    public int getQuantity()
+    {
+        return this.quantity;
+    }
+
+    public void setQuantity(int newQuantity)
+    {
+        this.quantity = newQuantity;
+    }
+
+    public int getReOrderLevel()   
+    {
+        return this.reOrderLevel;
+    }
+
+    public void setReOrderLevel(int newReOrderLevel)
+    {
+        this.reOrderLevel = newReOrderLevel;
+    }
+
+    public String toString()
+    {   
+        return "StockItem [ID: "+getItemID()+ " | DESCRIPTION: "+getItemDesc()+" | PRICE: "+getPrice()+" | QUANTITY: "+getQuantity()+" | RE-ORDER LEVEL: "+getReOrderLevel() + "]\n";
+    }
+
+    public String format()
+    {
+        return String.format("%-10s %-20s %-10.2f %-10d %-20d",
+        this.getItemID(), this.getItemDesc(), this.getPrice(), this.getQuantity(), this.getReOrderLevel());
+    }
+
 }
---
+++ assignmentTwo/StockItemTester2.java
@@ -0,0 +1,39 @@
+package assignmentTwo;
+import java.util.*;
+
+public class StockItemTester2
+{
+   public static void main(String[]args)
+   {
+    StockItem2 item1  = new StockItem2("AB123", "x100 Paku", 1.00, 20, 30);
+    StockItem2 item2 = new StockItem2("AC123", "x100 Rubber-Bands", 0.99, 50, 20);
+    StockItem2 item3 = new StockItem2("Tt432", "x30 Screwdriver",56.00, 15, 13);
+
+    System.out.println("\n--------------testing constructor-----------------");
+
+    System.out.println(item1); //testing the constructor
+    System.out.println(item2);
+
+    // item1.getItemID("EW123");//testing the sets
+    // item1.getItemDesc("x1 Hammer");
+
+    System.out.println("\n--------------testing sets-----------------");
+
+    item1.setPrice(9.00); //testing sets
+    item1.setQuantity(10);
+    item1.setReOrderLevel(10);
+    System.out.println(item1); //test the toString method
+
+    System.out.println("\n--------------testing gets-----------------\n");
+
+    System.out.println("ID: "+item2.getItemID()); //testint the gets
+    System.out.println("Description: "+item2.getItemDesc());
+    System.out.println("Price: "+item2.getPrice());
+    System.out.println("Quantity: "+item2.getQuantity());
+    System.out.println("Re-order level: "+item2.getReOrderLevel()); 
+
+    System.out.println("\n-----------testing toString and format-------------\n");
+    System.out.println(item3); //tests the toString
+    System.out.println(item3.format()+"\n"); //toSting
+   }
+}
