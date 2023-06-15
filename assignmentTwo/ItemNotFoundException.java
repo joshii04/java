5aa6f5b4ffad8db49b0f6cc420d294e5e742ba4f
joshii04 <joshua.sinclair04@gmail.com>
2023-06-15 11:36:30 +0700

    testing java save
    works well

--- assignmentTwo/ItemNotFoundException.java
+++ assignmentTwo/ItemNotFoundException.java
@@ -5,5 +5,6 @@ public class ItemNotFoundException extends Exception
         public ItemNotFoundException(String message)
         {
             super(message);//this is where the message will be outputed through 
+            //this class is very powerful, especially when you catch the errors as you please
         }    
     }
