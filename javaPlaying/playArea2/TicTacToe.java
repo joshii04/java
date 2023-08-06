fd42c25c52106d897de3cced2b9307b2d14075ac
joshii04 <joshua.sinclair04@gmail.com>
2023-08-06 12:58:33 +0100

    not done this block
    save

--- javaPlaying/playArea2/TicTacToe.java
+++ javaPlaying/playArea2/TicTacToe.java
@@ -15,16 +15,63 @@ public class TicTacToe{
         Scanner scanner = new Scanner(System.in);
         // ^ scanner object 
 
-        
+      
+
+        switch(userInput){
+            case "1":
+                board[0][0] = 'x';
+                break;
+            case "2":
+                board[0][1] = 'x';
+                break;   
+            case "3":
+                board[0][2] = 'x';
+                break;     
+            case "4":
+                board[1][0] = 'x';
+                break;
+            case "5":
+                board[1][1] = 'x';
+                break;   
+            case "6":
+                board[1][2] = 'x';
+                break; 
+            case "7":
+                board[2][0] = 'x';
+                break;
+            case "8":
+                board[2][1] = 'x';
+                break;   
+            case "9":
+                board[2][2] = 'x';
+                break;
+            default:
+                System.out.println("That was not a valid option");               
+        }
+
+        printBoard(board);
+
     }
 
     private static void printBoard (char[][] board) {
 
+        System.out.println(" ");
         System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);  
         System.out.println("-+-+-");    
         System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);  
         System.out.println("-+-+-");                
         System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+        System.out.println(" ");
 
     }
+
+    private static void getUserIn (board, scanner){
+        
+    }
+
+        System.out.println("What position would you like to play? 1-9 (left to right)");
+
+        String userInput = scanner.nextLine();
+
+        System.out.println(userInput);
 }
