67a7b9dbea364574067479b13ab49252f16e620c
joshii04 <joshua.sinclair04@gmail.com>
2023-08-11 22:10:07 +0100

    save

---
+++ javaPlaying/playArea3/JsBankInterface.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public interface JsBankInterface{
+    
+    public void addUser(JsBank name);
+
+    public void addpass(JsBank password);
+}
---
+++ javaPlaying/playArea3/JsBankLinkedList.java
@@ -0,0 +1,7 @@
+import java.io.*;
+import java.util.LinkedList;
+
+public class JsBankLinkedList implements JsBankInterface{
+
+    private LinkedList<JsBank> user;
+}
