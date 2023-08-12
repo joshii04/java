d11d453afe0619d2b52c1d8370b95f670bcc4d1a
joshii04 <joshua.sinclair04@gmail.com>
2023-08-12 17:56:02 +0100

    save
    save

--- javaPlaying/playArea3/BankAccount.java
+++ javaPlaying/playArea3/BankAccount.java
@@ -1,16 +1,57 @@
 public class BankAccount{
 
     private double balance;
-    private double initBalance;
-    private double withdraw;
-    private double deposit;
+    private String name;
+    private String number;
 
-    public BankAccount(double balance){
+    public BankAccount(String name, String number, double balance){
         this.balance = balance;
-        this.initBalance = initBalance;
-        this.withdraw = withdraw;
-        this.deposit = deposit;
+        this.name = name;
+        this.number = number;
     }
 
+    public void depositMoney(double newDeposit){
+        this.balance += newDeposit;
+        // increment balance
+        System.out.println("deposit success, new balance: "+this.balance);
+    }
+
+    public void withdraw(double newWithdraw){
+        if(this.balance - newWithdraw < 0){
+            System.out.println("withdraw unsucessful because only "+ this.balance+" is left" );
+        }
+        else{
+            this.balance -= newWithdraw;
+            System.out.println("Current balance is "+this.balance);
+        }
+    }
+
+    public double getBalance(){
+        return this.balance;
+    }
+
+    public void setBalance(double newBalance){
+        this.balance = newBalance;
+    }
+
+    public String getNumber(){
+        return this.number;
+    }
+
+    public void setNumber(String newNumber){
+        this.number = newNumber;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public String toString(){
+        return "Name: "+getName()+"|| Number: "+getNumber()+"|| Balance: "+getBalance();
+    }
 
 }
--- javaPlaying/playArea3/bankAccountTester.java
+++ javaPlaying/playArea3/bankAccountTester.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 public class bankAccountTester{
    public static void main(String[]args){
 
+        BankAccount joshAccount = new BankAccount("Josh", "CA12345", 0);
+
+        joshAccount.depositMoney(100);
+        joshAccount.depositMoney(150);
+        joshAccount.withdraw(200);
+        // System.out.println(joshAccount);
    }
 
 
