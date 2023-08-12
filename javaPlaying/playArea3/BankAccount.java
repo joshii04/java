78b8731282809e38df99d4dec1055d8df825448a
joshii04 <joshua.sinclair04@gmail.com>
2023-08-12 16:34:44 +0100

    save
    save

---
+++ javaPlaying/playArea3/BankAccount.java
@@ -0,0 +1,16 @@
+public class BankAccount{
+
+    private double balance;
+    private double initBalance;
+    private double withdraw;
+    private double deposit;
+
+    public BankAccount(double balance){
+        this.balance = balance;
+        this.initBalance = initBalance;
+        this.withdraw = withdraw;
+        this.deposit = deposit;
+    }
+
+
+}
---
+++ javaPlaying/playArea3/bankAccountTester.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class bankAccountTester{
+   public static void main(String[]args){
+
+   }
+
+
+
+} 
